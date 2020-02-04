a083fb8aa634336ab5ffa52710409fe73db80470
LOVETESH <luvteshchandra@gmail.com>
2020-02-04 20:14:54 +0530

    Added landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import {Route, link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path = '/' component = {landingpage()} />
+      <Route exact path = '/signup' component = {signuppage()} />
+      <Route exact path = '/profile' component = {profilepage()} />
+      <Route exact path = '/settings' component = {settingspage()} />
     </div>
   );
 }
+
+function landingpage(){
+  // Call firebase func to know if the user is logged in
+  return SignIn;
+}
+
+function signuppage(){
+  // Call firebase func to know if the user is logged in
+  return SignUp;
+}
+
+function settingspage(){
+  // Call firebase func to know if the user is logged in
+  return SignIn;
+}
+
+function profilepage(){
+  // Call firebase func to know if the user is logged in
+  return SignIn;
+}
+
 
 export default App;
